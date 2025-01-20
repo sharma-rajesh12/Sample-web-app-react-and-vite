63013105a050ec4f2d0795213c81cc7c5d1c00d8
sharma-rajesh12 <yadavrajesh1344@gmail.com>
2025-01-20 15:53:17 +0530

    Add Styled with package

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { MainApp } from './styled'
 
 function App() {
   const storageKeyName = "count"
@@ -24,14 +25,22 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => addNumber(count)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
+      </div> */}
+      <MainApp>
+        <button onClick={() => addNumber(count)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </MainApp>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/styled.js
@@ -0,0 +1,10 @@
+import  styled  from 'styled-components'
+
+export const MainApp = styled.body`
+background-color: purple;
+color: white;
+font-size: 32px; 
+display: flex;
+justify-content: center;
+width: 100vw;
+`
